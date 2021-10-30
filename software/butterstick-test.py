58f3323571e62fa6e3b6a318c9fa9c34ee84e1f4
Greg Davill <greg.davill@gmail.com>
2021-10-30 14:53:00 +1030

    Correctly configure and read ADC values

--- software/butterstick-test.py
+++ software/butterstick-test.py
@@ -3,6 +3,7 @@
 # This file is Copyright 2021 (c) Greg Davill <greg.davill@gmail.com>
 # License: BSD
 
+from struct import unpack
 import serial.tools.list_ports
 import serial
 import subprocess
@@ -13,6 +14,7 @@ import builtins
 import statistics
 import re
 from pyftdi import gpio,jtag
+from pyftdi.spi import SpiController
 from pyftdi.bits import BitSequence
 
 from time import sleep, localtime, strftime
@@ -112,42 +114,111 @@ load_bitstream()
 
 # Bitstream running, connect to it via JTAG
 
+def jtag_uart():
+    j = jtag.JtagEngine()
+    j.configure('ftdi:///1')
+    j.reset()
+    j.write_ir(BitSequence(0x32, msb=False, length=8))
+
+
+    while 1:
+        # shift out 2000 bits (this seems to work well with the buffers)
+        seq = BitSequence()
+        for n in range(200):
+            seq.append(BitSequence(0x001, msb=False, length=10))
+
+        j.change_state('shift_dr')
+        seq = j.shift_register(seq)
+        j.change_state('pause_dr')
+        
+        for n in range(200):
+            v = seq[10*n:10*(n+1)]
+            v.reverse()
+            if v[0]:
+                v.lsr(1)
+                b = v.tobytes(msb=True)
+                try:
+                    print(b.decode('ascii')[0], end='', flush=True)
+                    serial_log += [b]
+                except:
+                    ...
+        
+        print('', end='', flush=True)    
+    j.close()
+
+
 
 j = jtag.JtagEngine()
 j.configure('ftdi:///1')
 j.reset()
-j.write_ir(BitSequence(0x32, msb=False, length=8))
+
+f = j.controller.ftdi
+
+spi_clk = 0x02
+spi_cipo = 0x04
+spi_copi = 0x08
+spi_cs = 0x10
+
+def write_gpio(ftdi, data: int) -> bytes:
+    low_data = 0 & 0xFF
+    low_dir = 0x0b
+    high_data = (data) & 0xFF
+    high_dir = (0x1A) & 0xFF
+    return bytes([ftdi.SET_BITS_HIGH, high_data, high_dir])
+                
+
+def read_gpio(ftdi) -> bytes:
+    return bytes([ftdi.GET_BITS_HIGH])
 
 
-while 1:
-    # shift out ~4k bits
-    seq = BitSequence()
-    for n in range(200):
-        seq.append(BitSequence(0x001, msb=False, length=10))
 
-    j.change_state('shift_dr')
-    seq = j.shift_register(seq)
-    j.change_state('pause_dr')
+# Get GPIO port to manage extra pins, use A*BUS4 as GPO, A*BUS4 as GPI
+#gpio = spi.get_gpio()
+#gpio.set_direction(0x0200, 0x0200)
+
+# SPI transfer
+def xfer_byte(b) -> bytes:
+    r = bytes()
+    b = BitSequence(b, msb=True, length=8)
+    for bit in b:
+        if bit:
+            r += write_gpio(f, spi_copi)
+            r += write_gpio(f, spi_clk | spi_copi)
+        else:
+            r += write_gpio(f, 0)
+            r += write_gpio(f, spi_clk)
+        r += read_gpio(f)
+    return r
+
     
-    for n in range(200):
-        v = seq[10*n:10*(n+1)]
-        v.reverse()
-        if v[0]:
-            v.lsr(1)
-            b = v.tobytes(msb=True)
-            try:
-                print(b.decode('ascii')[0], end='')
-            except:
-                ...
-    #    seq.append(BitSequence(0x001 | (ord('a') << 1), msb=False, length=10))
 
 
-    #print(v)
-    
 
-    
-j.close()
+def read_adc(channel) -> float:
+    cmd = bytes()
+    cmd += write_gpio(f, 0)
+    cmd += xfer_byte(0x01)
+    cmd += xfer_byte(0x80 | (channel << 4))
+    cmd += xfer_byte(0x0)
+    cmd += write_gpio(f, 0)
+    cmd += write_gpio(f, spi_cs)
 
+    cmd += bytes([f.SEND_IMMEDIATE])
+
+    f.write_data(cmd)
+
+    data = f.read_data_bytes(24, 4)
+    value = 0
+    for b in data:
+        value = (value << 1) | (b & spi_cipo)
+    value >>= 2
+    value &= 0x00003FF
+    f.purge_buffers()
+    return value * (3.33 / 1024.0)
+
+for i in range(8):
+    print(f'{0} -> {read_adc(i):0.2f}')
+    ...
 
 
 finish("PASS")
